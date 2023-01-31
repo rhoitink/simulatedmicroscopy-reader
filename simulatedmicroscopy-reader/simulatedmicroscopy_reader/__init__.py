5c4336d032e61679fbe4355b0a16d8842a533958
Roy Hoitink <L.D.Hoitink@uu.nl>
2023-01-31 09:50:26 +0100

    Fix units of imported data

--- simulatedmicroscopy-reader/simulatedmicroscopy_reader/__init__.py
+++ simulatedmicroscopy-reader/simulatedmicroscopy_reader/__init__.py
@@ -1,11 +1,10 @@
 from typing import  List, Dict, Sequence, Union
-
+import napari
 from simulatedmicroscopy import Image
 from pathlib import Path
 
 PathLike = str
 PathOrPaths = Union[PathLike, Sequence[PathLike]]
-
 
 def get_reader(path: PathOrPaths):
     # If we recognize the format, we return the actual reader function
@@ -16,17 +15,29 @@ def get_reader(path: PathOrPaths):
 
 
 def h5_file_reader(path: PathOrPaths) -> List:
+    viewer = napari.current_viewer()
     p = Path(path)
     im = Image.load_h5file(path)
-    data = im.image.copy()  # somehow read data from path
-    ps = im.get_pixel_sizes()
-    scale = ps/ps.min()
-    layer_attributes = {"name": p.stem, "scale": scale}
+    data = im.image.copy()  # read data from file
+    pixel_size_um = im.get_pixel_sizes(unit="µm")
+
+    # set name and scale of image
+    layer_attributes = {"name": p.stem, "scale": pixel_size_um}
+
+    # add to list that will be returned
     return_data = [(data, layer_attributes)]
+
+    # if coords are present, include those as points
     if im.get_pixel_coordinates() is not None:
         # @todo: fix scaling 
-        coords_layer_attributes = {"name": "coords_"+p.stem, "scale": scale, "edge_color": "red", "face_color": "transparent", "size": [10, 10, 10]}
+        coords_layer_attributes = {"name": "coords_"+p.stem, "scale": pixel_size_um, "edge_color": "red", "face_color": "transparent", "size": 10.}
         coords = im.get_pixel_coordinates().copy()
+
+        # add as layer
         return_data.append((coords, coords_layer_attributes, 'points'))    
+    
+    # set correct scaling
+    viewer.scale_bar.visible = True
+    viewer.scale_bar.unit = "um"
     
     return return_data
